10998eb5f80367c6f76e19d5ea4b6606c934b817
LuciaOcana <lucia.ocana@estudiantat.upc.edu>
2024-11-28 10:09:53 +0100

    Gitignore Update

--- build/utils/error.handle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (res, error, errorRaw) => {
-    console.log(errorRaw);
-    res.status(500);
-    res.send({ error });
-};
-exports.handleHttp = handleHttp;
