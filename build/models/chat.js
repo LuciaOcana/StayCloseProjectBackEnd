ef0a9fafb04efcc8761d1a7e859657a898c7fe11
Roc Meseguer <roc.meseguer@upc.edu>
2024-11-28 10:15:46 +0100

    build and node_modules removed

--- build/models/chat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chatofDB = exports.chatSchema = void 0;
-const mongoose_1 = require("mongoose");
-exports.chatSchema = new mongoose_1.Schema({
-    chatType: { type: Boolean, required: true },
-    participants: [{ type: String }]
-});
-exports.chatofDB = (0, mongoose_1.model)('chat', exports.chatSchema);
