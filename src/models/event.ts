6c2b6b4eb3958d51f7f8df68e76bd9a228b40767
CarlaAbascal <carla.abascal@estudiantat.upc.edu>
2024-12-10 13:04:42 +0100

    Calendari ready

--- src/models/event.ts
+++ src/models/event.ts
@@ -5,7 +5,7 @@ export interface eventInterface{
    //eventID: string, *¿PONEMOS UNA ID NOSOTRAS O ESCOGEMOS LA QUE ASIGNA MONGO POR DEFECTO?* 
    name: string,
    description: string,
-   date: Date,
+   eventDate: Date,
    ubication: [],
    creator: Schema.Types.ObjectId
 }
@@ -15,7 +15,7 @@ export type newEventInfo = Omit<eventInterface,'id'>
 export const eventSchema = new Schema<eventInterface>({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    date: { type: Date, required: true },
+    eventDate: { type: Date, required: true },
     ubication: [{ type: String }],
     creator: { type: Schema.Types.ObjectId, ref: 'user', required: true }
 })
